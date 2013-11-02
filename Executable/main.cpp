6734469618bcea0dd215bf7edfb9882de2a7c3eb
Hyeon Kim <uie10@naver.com>
2013-11-03 07:56:34 +0900

    그림 그리는 코드 추가

--- Executable/main.cpp
+++ Executable/main.cpp
@@ -3,8 +3,48 @@
 
 
 
+inline int ARGB(BYTE A, BYTE R, BYTE G, BYTE B)
+{
+    return (A << 24) | (R << 16) | (G << 8) | B;
+}
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-    SystemClass window(hInstance, 800, 600, false);
+    size_t width = 800;
+    size_t height = 600;
+
+
+    // ºñÆ®¸Ê Á¤º¸ÆÄÀÏ
+    BITMAPINFO info = {};
+    info.bmiHeader.biSize = sizeof BITMAPINFOHEADER;
+    info.bmiHeader.biWidth = width;
+    info.bmiHeader.biHeight = height;
+    info.bmiHeader.biPlanes = 1;
+    info.bmiHeader.biBitCount = 32;
+    info.bmiHeader.biCompression = BI_RGB;
+
+    // ºñÆ®¸Ê ¹öÆÛ
+    unique_ptr<int[]> buffer(new int[width*height]);
+    unique_ptr<int*[]> screen(new int*[height]);
+    for( size_t i=0; i<height; ++i ) screen[i] = &buffer[i*width];
+
+
+    // Ã¢ ¶ç¿ì±â
+    SystemClass window(hInstance, width, height, false);
+    window.onDraw = [&info, &screen](HDC hdc)
+    {
+        size_t width = info.bmiHeader.biWidth;
+        size_t height = info.bmiHeader.biHeight;
+
+        for( size_t y=0; y<height; ++y )
+        {
+            for( size_t x=0; x<width; ++x )
+            {
+                screen[y][x] = 0x47a6ff;
+            }
+        }
+
+        SetDIBitsToDevice(hdc, 0, 0, width, height, 0, 0, 0, height, &screen[0][0], &info, DIB_RGB_COLORS);
+    };
     window.Run(nCmdShow);
 }
