576b7d0017a68c601be9be343edc49c18d5331e6
Hyeon Kim <uie10@naver.com>
2013-11-28 13:26:09 +0900

    naive scanline conversion algorithm implement

--- Executable/main.cpp
+++ Executable/main.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 
+using namespace std;
+using namespace DirectX;
 using namespace tbb;
 
 
@@ -9,11 +11,14 @@ inline int ARGB(BYTE A, BYTE R, BYTE G, BYTE B)
     return (A << 24) | (R << 16) | (G << 8) | B;
 }
 
+inline int ARGB(BYTE R, BYTE G, BYTE B)
+{
+    return (R << 16) | (G << 8) | B;
+}
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-    size_t width = 800;
-    size_t height = 600;
-
+    size_t width = 800, height = 600;
 
     // ºñÆ®¸Ê Á¤º¸ÆÄÀÏ
     BITMAPINFO info = {};
@@ -25,28 +30,64 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
     info.bmiHeader.biCompression = BI_RGB;
 
     // ºñÆ®¸Ê ¹öÆÛ
+    XMFLOAT4A A(380, 130, 0, 1);
+    XMFLOAT4A B(170, 360, 0, 1);
+    XMFLOAT4A C(600, 400, 0, 1);
+    if (A.y > B.y) swap(A, B);
+    if (A.y > C.y) swap(A, C);
+    if (B.y > C.y) swap(B, C);
+
+    XMVECTOR a = XMLoadFloat4A(&A);
+    XMVECTOR b = XMLoadFloat4A(&B);
+    XMVECTOR c = XMLoadFloat4A(&C);
+    XMVECTOR ab = b - a, bc = c - b, ca = a - c;
+    XMFLOAT4A AB, BC, CA;
+    XMStoreFloat4A(&AB, ab);
+    XMStoreFloat4A(&BC, bc);
+    XMStoreFloat4A(&CA, ca);
+    
+    
+
     unique_ptr<int[]> buffer(new int[width*height]);
     unique_ptr<int*[]> screen(new int*[height]);
-    for( size_t i=0; i<height; ++i ) screen[i] = &buffer[i*width];
-
+    for (size_t i = 0; i < height; ++i) screen[i] = &buffer[i*width];
 
     // Ã¢ ¶ç¿ì±â
     WindowDesc desc;
     desc.WindowTitle(L"Multi-threaded Software Rasterizer!");
     desc.WindowSize(width, height);
     GdiWindow window(desc);
-    window.setDraw([&info, &screen](HDC hdc)
+    window.setDraw([&](HDC hdc)
     {
-        size_t width = info.bmiHeader.biWidth;
-        size_t height = info.bmiHeader.biHeight;
-
-        parallel_for( blocked_range<size_t>(0, height), [=, &screen](const blocked_range<size_t>& r)
+        parallel_for(blocked_range<size_t>(0, height), [&](const blocked_range<size_t>& r)
         {
-            for( size_t y=r.begin(); y<r.end(); ++y )
+            for (size_t y = r.begin(); y < r.end(); ++y)
             {
-                for( size_t x=0; x<width; ++x )
+                float AB_X = AB.x / AB.y * (y - A.y) + A.x;
+                float BC_X = BC.x / BC.y * (y - B.y) + B.x;
+                float CA_X = CA.x / CA.y * (y - C.y) + C.x;
+
+                function<void()> code;
+                if (A.y <= y && y < B.y)
                 {
-                    screen[y][x] = GetCurrentThreadId();
+                    for (size_t x = 0; x < width; ++x)
+                    {
+                        screen[y][x] = ((CA_X <= x && x < AB_X) || (AB_X <= x && x <CA_X)) ? 0x4a8eff : 0xffffff;
+                    }
+                }
+                else if (B.y <= y && y< C.y)
+                {
+                    for (size_t x = 0; x < width; ++x)
+                    {
+                        screen[y][x] = ((CA_X <= x && x < BC_X) || (BC_X <= x && x <CA_X)) ? 0x4a8eff : 0xffffff;
+                    }
+                }
+                else
+                {
+                    for (size_t x = 0; x < width; ++x)
+                    {
+                        screen[y][x] = 0xffffff;
+                    }
                 }
             }
         });
